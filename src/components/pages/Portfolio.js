5e9c0380d4b579c3a1b672ce341331dc10244fc6
Isaac Luton <isaacdluton@gmail.com>
2022-10-30 21:43:01 -0700

    project page images resize

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,67 +1,64 @@
-import zenten from "../../images/zenten.png";
-import weather from "../../images/weather.png";
-import devise from "../../images/devise.png";
-import tech from "../../images/tech.png";
-import stop from "../../images/stop.png";
+import zenten from "../../images/zenten.jpeg";
+import weather from "../../images/weather.jpeg";
+import devise from "../../images/devise.jpeg";
+import tech from "../../images/tech.jpeg";
+import stop from "../../images/stop.jpeg";
 
 export default function Portfolio() {
   return (
     <main className="mainPortfolio">
       <div className="portfolioContainer">
-        <div className="portfolioOne">
-          <a
-            href="https://juliakhdavis.github.io/zenTen/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="projectImage"
-              src={zenten}
-              alt="code refactor word mashup"
-            />
-          </a>
-          <a
-            href="https://devisehub.herokuapp.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="projectImage" src={devise} alt="react logo" />
-          </a>
-        </div>
-        <div className="portfolioTwo">
-          <a
-            href="https://support-pandemic-obliteration.herokuapp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="projectImage" src={stop} alt="stop website" />
-          </a>
-          <a
-            href="https://thawing-fortress-77603.herokuapp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="projectImage"
-              src={tech}
-              alt="the word blog coming of of computer hardware"
-            />
-          </a>
-        </div>
-        <div className="portfolioThree">
-          <a
-            href="https://iluton.github.io/Weather-Dashboard/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="projectImage"
-              src={weather}
-              alt="weather dashboard diplaying weather"
-            />
-          </a>
+        <a
+          href="https://juliakhdavis.github.io/zenTen/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="projectImage"
+            src={zenten}
+            alt="code refactor word mashup"
+          />
+        </a>
 
-        </div>
+        <a
+          href="https://devisehub.herokuapp.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="projectImage" src={devise} alt="react logo" />
+        </a>
+
+        <a
+          href="https://support-pandemic-obliteration.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="projectImage" src={stop} alt="stop website" />
+        </a>
+
+        <a
+          href="https://thawing-fortress-77603.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="projectImage"
+            src={tech}
+            alt="the word blog coming of of computer hardware"
+          />
+        </a>
+
+        <a
+          href="https://iluton.github.io/Weather-Dashboard/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="projectImage"
+            src={weather}
+            alt="weather dashboard diplaying weather"
+          />
+        </a>
       </div>
     </main>
   );
