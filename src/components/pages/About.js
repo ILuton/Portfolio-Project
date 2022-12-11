789d0d96ac567408bf54c5ecb3f35d99f9d684c2
Isaac Luton <isaacdluton@gmail.com>
2022-12-10 20:16:40 -0800

    fix typo and update about text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import sunrise from "../../video/sunrise.mp4"
+import sunrise from "../../video/sunrise.mp4";
 
 export default function About() {
   const styles = {
@@ -11,15 +11,14 @@ export default function About() {
       overflow: "auto",
     },
     fontStyle: {
-      margin: "3rem",
-      fontSize: "1.5vw"
+      margin: "2rem",
+      fontSize: "1.5vw",
     },
     videoDiv: {
       position: "fixed",
       zIndex: "-1",
       width: "100%",
       height: "100%",
-
     },
     videoStyle: {
       objectFit: "cover",
@@ -27,31 +26,38 @@ export default function About() {
       height: "100vh",
       position: "fixed",
       top: "0",
-      left: "0"
-
-    }
-
+      left: "0",
+    },
   };
 
   return (
     <section className="aboutCon" style={styles.containerStyle}>
       <div style={styles.videoDiv}>
-        <video  style={styles.videoStyle} src={sunrise} autoPlay loop muted></video>
+        <video
+          style={styles.videoStyle}
+          src={sunrise}
+          autoPlay
+          loop
+          muted
+        ></video>
       </div>
       <div style={styles.fontStyle}>
         <p className="aboutText">
-          I am a Seattle-based full stack Web Developer. After graduating from
-          Washington State University, I launched my career in healthcare -
-          doubling as a Personal Trainer and Physical Therapy Tech. I discovered
-          a passion for building and designing web pages and took the leap into
-          the tech world. I started learning on my own and continued through the
-          University of Washington Coding Bootcamp. I am excited to expand my
-          skillset while contributing to a web development team.
+          I am a Seattle-based Full Stack Web Developer proficient in
+          JavaScript, HTML, CSS, React, MySQL, MongoDB. After graduating from
+          Washington State University, I started my career in healthcare -
+          doubling as a Personal Trainer and Physical Therapy Tech. Although I
+          enjoyed working with people, I discovered my true passion for software
+          development after working on a webpage design project. I made the leap
+          into the tech world and started learning coding skills on my own. I
+          furthered my education through completion of the rigorous University
+          of Washington Coding Bootcamp. I am excited to launch my career in
+          tech while contributing to a web development team.
         </p>
 
         <p className="aboutText">
           You can typically find me lifting weights at the gym, enjoying the
-          great oudoors of the PNW, or trying my hand at making sourdough pizza.
+          great outdoors of the PNW, or trying my hand at making sourdough pizza.
         </p>
       </div>
     </section>
